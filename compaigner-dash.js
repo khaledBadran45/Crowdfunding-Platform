dc66154a3135913a4ec0bd42af543436746e1904
khaledBadran45 <kBadran842@gmail.com>
2025-05-17 00:35:34 +0300

    add ui of create app  of compaigner dashbord

--- compaigner-dash.js
+++ compaigner-dash.js
@@ -1,4 +1,31 @@
-export class Campaigner {
+const base64Images = [];
+document
+  .querySelector("#fileImages")
+  .addEventListener("change", function (event) {
+    const files = event.target.files;
+
+    [...files].forEach((file) => {
+      const reader = new FileReader();
+
+      reader.readAsDataURL(file);
+      reader.onload = function (e) {
+        const fullBase64 = e.target.result;
+        const base64Decoded = fullBase64.split(",")[1]; // Remove data prefix
+        base64Images.push(base64Decoded);
+        console.log(base64Images);
+      };
+      reader.onerror = function (err) {
+        console.log("error while reading files: " + err);
+      };
+    });
+  });
+
+class Campaigner {
+  #createCompaignPage = "";
+  #compaignDetailsUpdate = "";
+  #compaignNewsUpdate = "";
   constructor() {}
-  
+  createCompaign() {}
+  compaignDetailsUpdate() {}
+  compaignNewsUpdate() {}
 }
