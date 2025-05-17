dde1064ca01f7d0e29cd3fe1d23b7d6089a2fa47
khaledBadran45 <kBadran842@gmail.com>
2025-05-18 01:04:29 +0300

    Comapigner edit Finish

--- compaigner-dash.js
+++ compaigner-dash.js
@@ -108,28 +108,88 @@ class Campaigner {
         </div> 
       </form>
   `;
+  #compaigns = [];
   #compaignDetailsUpdate = "";
   #compaignNewsUpdate = "";
   #createCompaignForm;
-  static compaignImages = [];
+  compaignImages = [];
   #compaignRewards = [];
   constructor() {
+    // setup Upadtion copmaigns
+    // patch the compaigns to the input
+    // get the input then attach the compaigns
+    this.fetchCurrentCompaigns();
+    document.querySelector(".compaignRow").addEventListener("click", (e) => {
+      if (e.target.dataset.id) {
+        const compaign = this.#compaigns.find((cop) => {
+          cop.id == e.target.dataset.id && this.patchTheCampaigns(cop);
+        });
+      }
+    });
     this.#compaignRewards = [];
     // console.log(this.#compaignRewards);
     this.compaignImages = [];
-    document.querySelector(".mainContent").innerHTML = this.#createCompaignPage;
-    this.#createCompaignForm = document.querySelector("form");
-    console.log(this.#createCompaignForm);
-    this.getListOfimagesinBase64();
-    this.#createCompaignForm.addEventListener("submit", (e) => {
-      this.createCompaign(e);
-    });
+    // setUp Creation Page
+    // document.querySelector(".mainContent").innerHTML = this.#createCompaignPage;
+    // this.#createCompaignForm = document.querySelector("form");
+    // // this.getListOfimagesinBase64();
+    // this.#createCompaignForm.addEventListener("submit", (e) => {
+    //   this.createCompaign(e);
+    // });
+    // document
+    //   .querySelector('input[name="addReward"]')
+    //   .addEventListener("click", () => {
+    //     this.addReward();
+    //   });
+  }
+
+  patchTheCampaigns(campaign) {
+    const titleInput = document.querySelector('input[name="title"]');
+    titleInput.value = campaign.title;
+
+    const descriptionInput = document.querySelector(
+      'input[name="Description"]'
+    );
+    descriptionInput.value = campaign.description;
+
+    const deadlineInput = document.querySelector('input[name="deadline"]');
+    deadlineInput.value = campaign.deadline;
+
+    const goalInput = document.querySelector('input[name="goal"]');
+    goalInput.value = campaign.goal;
+    const Images = this.compaignImages;
+
+    const newsInp = document.querySelector('input[name="news"]');
+    newsInp.value = campaign.value;
     document
-      .querySelector('input[name="addReward"]')
-      .addEventListener("click", () => {
-        this.addReward();
+      .querySelector("input[name='patch']")
+      .addEventListener("click", function () {
+        const title = titleInput.value;
+        const description = descriptionInput.value;
+        const deadline = deadlineInput.value;
+        const goal = goalInput.value;
+        const cam = new compaign(
+          title,
+          description,
+          goal,
+          deadline,
+          [],
+          [],
+          newsInp.value
+        );
+        fetch(`http://localhost:3000/compaigns/${campaign.id}`, {
+          body: JSON.stringify(cam),
+          method: "PATCH",
+        })
+          .then((res) => {
+            alert("successfully patch the Campaign with id  " + campaign.id);
+          })
+          .catch((res) => {
+            console.log(res);
+          });
       });
   }
+
   addReward() {
     const Reward_Title = document
       .querySelector('input[name="Reward_Title"]')
@@ -138,6 +198,7 @@ class Campaigner {
     // why this compoins rewards is return undefined
     this.#compaignRewards.push({ reward_Title: Reward_Title, limit: Limit });
     console.log(this.#compaignRewards);
+    alert("you add 1 reward to the current compaign");
   }
   getImages() {
     return Campaigner.compaignImages;
@@ -149,6 +210,7 @@ class Campaigner {
       document.querySelector("input[name='Description']").value.trim(),
       document.querySelector("input[name='Goal']").value.trim(),
       document.querySelector("input[name='Deadline']").value.trim(),
+      "The campign just starting now",
       this.#compaignRewards,
       this.getImages()
     );
@@ -165,7 +227,19 @@ class Campaigner {
 
     console.log(comapign);
   }
-  compaignDetailsUpdate() {}
+  fetchCurrentCompaigns() {
+    fetch("http://localhost:3000/compaigns")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        this.#compaigns = data;
+        this.displayCompaigns(data);
+      })
+      .catch((x) => {
+        console.log(x);
+      });
+  }
   compaignNewsUpdate() {}
   getListOfimagesinBase64() {
     document
@@ -175,9 +249,8 @@ class Campaigner {
         [...files].forEach((file) => {
           const reader = new FileReader();
           reader.readAsDataURL(file);
-          reader.onload = function (e) {
+          reader.onload = (e) => {
             const fullBase64 = e.target.result; // Remove data prefix
-            console.log(fullBase64);
             console.log(fullBase64);
             const img = document.createElement("img");
             img.src = fullBase64;
@@ -186,7 +259,9 @@ class Campaigner {
             document
               .querySelector(".uplaoded_images_contaienr")
               .appendChild(img);
-            Campaigner.compaignImages.push(fullBase64);
+            // want this to be on the claass
+
+            this.compaignImages.push(fullBase64);
           };
           reader.onerror = function (err) {
             console.log("error while reading files: " + err);
@@ -194,10 +269,81 @@ class Campaigner {
         });
       });
   }
+
+  displayCompaigns(listOfCompaign) {
+    listOfCompaign.forEach((comp) => {
+      document.querySelector(
+        ".compaignRow"
+      ).innerHTML += `<div class="col-lg-4 col-md-6 col-sm-12 p-2">
+                <div class="d-flex justify-content-center">
+                  <div
+                    class="card shadow-sm position-relative overflow-hidden"
+                    id="campaignCard"
+                  >
+                    <!-- Campaign Image -->
+                    <div class="position-relative">
+                      <img
+                        id="campaignImage"
+                        class="img-fluid"
+                        alt="Campaign Cover"
+                        src="${comp.images[0]}"
+                        style="height: 200px; object-fit: cover; width: 300px"
+                      />
+                    </div>
+  
+                    <!-- Card Body -->
+                    <div class="card-body text-start p-3">
+                      <h5 id="campaignTitle" class="fw-bold mb-2 text-capitalize">
+                        ${comp.title}
+                      </h5>
+                      <p
+                        id="campaignDescription"
+                        class="text-muted text-capitalize small"
+                      >
+                       ${comp.description}
+                      </p>
+                      <div class="mb-3">
+                        <!-- <div
+                          class="progress"
+                          role="progressbar"
+                          aria-valuemin="0"
+                          aria-valuemax="100"
+                        >
+                          <div
+                            id="campaignProgress"
+                            class="progress-bar"
+                            style="height: 10px"
+                          ></div>
+                        </div> -->
+                        <div
+                          class="d-flex justify-content-between align-items-center"
+                        >
+                          <p class="fw-bold text-muted">Goal</p>
+                          <p class="fw-semibold text-muted">${comp.goal}</p>
+                        </div>
+                        <div
+                          class="d-flex justify-content-between align-items-center"
+                        >
+                          <p class="fw-bold text-muted">Deadline</p>
+                          <p class="fw-semibold text-danger">${comp.deadline}</p>
+                        </div>
+                        <div class="text-end">
+                          <a class="editBtn">
+                            <i class="fa-solid fa-pen" data-id='${comp.id}'></i>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>`;
+    });
+  }
 }
+
 new Campaigner();
 class compaign {
-  constructor(title, description, goal, deadline, rewards, images) {
+  constructor(title, description, goal, deadline, lastNews, rewards, images) {
     this.title = title;
     this.description = description;
     this.goal = goal;
@@ -205,5 +351,6 @@ class compaign {
     this.rewards = rewards;
     this.images = images;
     this.isApproved = false;
+    lastNews = lastNews;
   }
 }
